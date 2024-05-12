1662a6ec5bb545c3d2c14b693747f519ff64fb94
Alpaca92 <zxcvbnm5288@gmail.com>
2024-05-13 08:41:23 +0900

    #3.3 Modifiers (10:06)

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 export default function Home() {
   return (
-    <main className='flex items-center justify-center h-screen p-5 bg-gray-300'>
-      <div className='w-full p-5 bg-white shadow-lg rounded-2xl'>
+    <main className='flex items-center justify-center h-screen p-5 bg-gray-100 dark:bg-gray-700'>
+      <div className='w-full p-5 bg-white shadow-lg rounded-3xl max-w-screen-sm dark:bg-gray-500'>
         <div className='flex items-center justify-between'>
           <div className='flex flex-col'>
-            <span className='-mb-1 font-semibold text-gray-600'>
+            <span className='-mb-1 font-semibold text- dark:text-gray-300'>
               In transit
             </span>
-            <span className='text-4xl font-semibold'>Cool blue</span>
+            <span className='text-4xl font-semibold dark:text-gray-400'>
+              Cool blue
+            </span>
           </div>
           <div className='bg-orange-400 rounded-full size-12' />
         </div>
         <div className='flex items-center gap-2 my-2'>
-          <span className='px-2.5 py-1.5 text-xs font-medium text-white uppercase bg-green-400 rounded-full'>
+          <span className='px-2.5 py-1.5 text-xs font-medium text-white uppercase bg-green-400 rounded-full hover:bg-green-500 hover:scale-125 transition'>
             Today
           </span>
           <span className=''>9:30 - 10:30</span>
